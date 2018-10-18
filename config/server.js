87424ba44824fd37bf09a042d1e20200ebb2f5db
Matheus Lauffer <matheuslauffer1@gmail.com>
2018-10-17 21:03:53 -0300

    Node e dependencias

---
+++ config/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var consign = require('consign');
+var bodyParser = require('body-parser');
+var expressSession = require('express-session');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(expressSession({
+    secret: "teste4all",
+    resave: false,
+    saveUnitialized: false
+}));
+
+app.use(function(req, res, next){
+    res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+      res.setHeader("Access-Control-Allow-Headers", "content-type");
+      res.setHeader("Access-Control-Allow-Credentials", true);
+  
+      next();
+    });
+
+consign().include('app').into(app);
+
+module.exports = app;
+
+
